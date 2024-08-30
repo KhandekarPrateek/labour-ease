0c362deb0464bd56df96aef78015605bd70c2c7a
Purvi Porwal <purviporwal1812@gmail.com>
2024-08-30 14:16:15 +0530

    conatct page css added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,8 +1,8 @@
-"use client";
+'use client'
 import React, { useRef } from "react";
 import emailjs from "@emailjs/browser";
-import Image from "next/image"; // Assuming you'll add an image
-import "./page.css"; // External CSS file
+import Image from 'next/image';
+import "./page.css"
 
 export default function Contact() {
   const form = useRef();
@@ -25,10 +25,10 @@ export default function Contact() {
   };
 
   return (
-    <div className="container-fluid contact-container">
-      <div className="row h-100 align-items-center">
-        <div className="col-sm-8">
-          <h1>Contact Us</h1>
+    <div className="container contact-container">
+      <div className="row h-100 align-items-center justify-content-center">
+        <div className="col-md-12 col-lg-6">
+          <h1 className="display-4">Contact Us</h1>
           <form ref={form} onSubmit={sendEmail}>
             <div className="mb-3">
               <label htmlFor="user_name" className="form-label">
@@ -37,7 +37,9 @@ export default function Contact() {
               <input
                 type="text"
                 name="user_name"
+                id="user_name"
                 className="form-control"
+                placeholder="Enter your name"
                 required
               />
             </div>
@@ -48,7 +50,9 @@ export default function Contact() {
               <input
                 type="email"
                 name="user_email"
+                id="user_email"
                 className="form-control"
+                placeholder="Enter your email"
                 required
               />
             </div>
@@ -58,22 +62,27 @@ export default function Contact() {
               </label>
               <textarea
                 name="message"
+                id="message"
                 className="form-control"
-                rows="4"
+                rows="6"
+                placeholder="Enter your message"
                 required
               />
             </div>
-            <input type="submit" value="Send" className="btn btn-primary" />
+            <button type="submit" className="btn btn-primary">
+              Send
+            </button>
           </form>
         </div>
-        <div className="col-sm-4 d-none d-sm-block">
+        <div className="col-md-12 col-lg-6 d-none d-md-block">
           <Image
-            src="/public/images/Header.png" // Replace with your image path
-            alt="Contact Us Image"
-            width={400}
-            height={400}
-            layout="responsive"
-          />
+        src="/images/contact.avif" 
+        alt="Contact Us"
+        layout="responsive" 
+        width={700} 
+        height={475} 
+        className="img-fluid" 
+      />
         </div>
       </div>
     </div>

0051e2c074a0582c3e16f7e6d833781e792c2356
Purvi Porwal <purviporwal1812@gmail.com>
2024-11-20 14:15:27 +0530

    page rating

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,6 @@ import { sql } from "@vercel/postgres";
 import Home from "./components/Home";
 
 export default async function Main() {
-<<<<<<< HEAD
-=======
-  
->>>>>>> 29972b6b2b7e56440faafff6141d689a17bc822c
   return (
     <main>
       <Home />

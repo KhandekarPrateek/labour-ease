0a092b3574ffdfcc3428d4d7249d3efefd26f19c
Purvi Porwal <purviporwal1812@gmail.com>
2025-01-02 20:36:50 +0530

    css updated for emp and shopkeeper

--- src/app/shopkeeper-dashboard/_components/shopkeeperDashboard.js
+++ src/app/shopkeeper-dashboard/_components/shopkeeperDashboard.js
@@ -142,7 +142,7 @@ const ShopkeeperDashboard = () => {
                 />
             ) : (
                 <>
-                    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                    <nav className="navbar navbar-expand-lg">
                         <div className="container-fluid">
                             <a className="navbar-brand" href={`/shopkeeper-dashboard?userID=${userID}`}>Dashboard</a>
                             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
